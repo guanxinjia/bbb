d6837b7edb899abc49d5e0c8caba8a43efac0283
= <454855664@qq.com>
2024-05-24 16:06:45 +0800

    git test

--- src/main/java/com/mybatisplus/dto/TestDTO.java
+++ src/main/java/com/mybatisplus/dto/TestDTO.java
@@ -4,7 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-
+//test
 @Data
 public class TestDTO {
     @TableId(type = IdType.AUTO)
